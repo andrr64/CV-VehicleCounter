8cd26aa348815d0e5fe979662d2fbbad8947ebca
andrr64 <derzandrr03@gmail.com>
2024-05-15 23:52:15 +0700

    add ui_util.py

---
+++ ui_util.py
@@ -0,0 +1,8 @@
+def printLine():
+    print(f"+{'-'*50}+")
+    
+def printCenter(text: str):
+    print("|"+ text.center(50) + "|")
+
+def printLeft(text: str):
+    print("| " + text.ljust(49) + "|")
