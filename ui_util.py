5173e7454ca27b4e4ec55c47a9c0d6ed30725ace
andrr64 <derzandrr03@gmail.com>
2024-05-22 03:33:11 +0700

    add new parameter: length

--- ui_util.py
+++ ui_util.py
@@ -1,8 +1,8 @@
 def printLine(length= 50):
     print(f"+{'-'*length}+")
     
-def printCenter(text: str):
-    print("|"+ text.center(50) + "|")
+def printCenter(text: str, length = 50):
+    print("|"+ text.center(length) + "|")
 
-def printLeft(text: str):
-    print("| " + text.ljust(49) + "|")
+def printLeft(text: str, length = 50):
+    print("| " + text.ljust(length-1) + "|")
